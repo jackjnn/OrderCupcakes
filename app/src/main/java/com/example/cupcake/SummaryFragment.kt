9bf5ca6b873a3a0c6859973d65eb3e746136cecf
jackjnn <jacknyangenj.jn2@gmail.com>
2021-04-23 00:38:08 +0300

    Cupcake order can now be sent to another app eg gmail. Also implicit intent to check event handling added to avoid crashing.

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -15,16 +15,16 @@
  */
 package com.example.cupcake
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentSummaryBinding
 import com.example.cupcake.model.OrderViewModel
-import androidx.navigation.fragment.findNavController
 
 /**
  * [SummaryFragment] contains a summary of the order details with a button to share the order
@@ -61,8 +61,29 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        val orderSummary = getString(
+                R.string.order_details,
+                sharedViewModel.quantity.value.toString(),
+                sharedViewModel.flavor.value.toString(),
+                sharedViewModel.date.value.toString(),
+                sharedViewModel.price.value.toString()
+        )
+
+
+
+        /**Implicit intent for sharing with another app*/
+
+        val intent = Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+                .putExtra(Intent.EXTRA_TEXT, orderSummary)
+
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+
+        }
     }
+
 
     /**
      * This fragment lifecycle method is called when the view hierarchy associated with the fragment
