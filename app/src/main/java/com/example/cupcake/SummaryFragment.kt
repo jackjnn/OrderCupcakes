45f53c9a081d3c70beac0eb156d8eb494758d89c
jackjnn <jacknyangenj.jn2@gmail.com>
2021-04-23 00:52:44 +0300

    sendorder() method updated

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -61,17 +61,14 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
         val orderSummary = getString(
                 R.string.order_details,
-                sharedViewModel.quantity.value.toString(),
+                resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
                 sharedViewModel.flavor.value.toString(),
                 sharedViewModel.date.value.toString(),
                 sharedViewModel.price.value.toString()
         )
-
-
-
-        /**Implicit intent for sharing with another app*/
 
         val intent = Intent(Intent.ACTION_SEND)
                 .setType("text/plain")
@@ -80,7 +77,6 @@ class SummaryFragment : Fragment() {
 
         if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
             startActivity(intent)
-
         }
     }
 
